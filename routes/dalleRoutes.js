2ada8cd1eeded01c33071aa8cf9407cb28d5b582
asfand687 <asfandyar687@gmail.com>
2023-02-05 23:06:07 +0500

    added new api key

--- routes/dalleRoutes.js
+++ routes/dalleRoutes.js
@@ -7,7 +7,7 @@ dotenv.config()
 const router = express.Router()
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY
+  apiKey: process.env.OPENAI_API_KEY_V2
 })
 
 const openai = new OpenAIApi(configuration)
