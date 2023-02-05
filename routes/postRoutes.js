5c161621801ac5cc47b305e4c9b26ee89fc13755
asfand687 <asfandyar687@gmail.com>
2023-02-05 23:56:28 +0500

    added the indents

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -28,14 +28,14 @@ router.route('/').get(async (req, res) => {
 router.route('/').post(async (req, res) => {
   try {
     const { name, prompt, photo } = req.body
-  const photoUrl = await cloudinary.uploader.upload(photo)
+    const photoUrl = await cloudinary.uploader.upload(photo)
 
-  const newPost = await Post.create({
-    name,
-    prompt,
-    photo: photoUrl.url
-  })
-
+    const newPost = await Post.create({
+      name,
+      prompt,
+      photo: photoUrl.url
+    })
+  
   res.status(201).json({ success: true, data: newPost })
   } catch (error) {
     res.status(500).json({ success: false, message: error })
